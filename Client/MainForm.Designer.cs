46f0eed0fcc7f4d47551ce4decd7befee9187871
Vincenzo Sciarra <17055610+reikor0x@users.noreply.github.com>
2022-09-26 17:24:41 +0200

    Add project files.

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TransictionScript
+{
+    partial class TransictionScriptClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(166, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Ora sei pronto per la tua migrazione a O365!\n\nSe hai ricevuto la mail che ti info" +
+    "rmava della migrazione, prosegui!";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(320, 328);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Sono sicuro!";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 223);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(612, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // TransictionScriptClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "TransictionScriptClient";
+            this.Text = "Transiction Script Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Client/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TransictionScript
+{
+    public partial class TransictionScriptClient : Form
+    {
+        public TransictionScriptClient()
+        {
+            InitializeLogs();
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (var process in Process.GetProcessesByName("Teams"))
+            {
+                WriteLog("Killing Teams process", "INFO");
+                process.Kill();
+            }
+
+            foreach (var process in Process.GetProcessesByName("Outlook"))
+            {
+                WriteLog("Killing Outlook process", "INFO");
+                process.Kill();
+            }
+
+            foreach (var process in Process.GetProcessesByName("WINWORD"))
+            {
+                WriteLog("Killing WINWORD process", "INFO");
+                process.Kill();
+            }
+
+
+            foreach (var process in Process.GetProcessesByName("Excel"))
+            {
+                WriteLog("Killing Excel process", "INFO");
+                process.Kill();
+            }
+
+            foreach (var process in Process.GetProcessesByName("PowerPoint"))
+            {
+                WriteLog("Killing PowerPoint process", "INFO");
+                process.Kill();
+            }
+
+            foreach (var process in Process.GetProcessesByName("OneNote"))
+            {
+                WriteLog("Killing OneNote process", "INFO");
+                process.Kill();
+            }
+
+
+
+            
+
+
+
+            DeleteKey("Software\\Microsoft\\OneDrive");
+            DeleteKey("Software\\Policies\\OneDrive");
+            DeleteKey("Software\\Microsoft\\Office\\16.0\\Common\\Identity");
+            DeleteKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace");
+            DeleteKey("Software\\SyncEngines\\Providers\\OneDrive");
+            DeleteKey("Software\\Classes\\CLSID\\{04271989-C4D2-8D26-E559-3BE1753BC3ED}");
+            DeleteKey("Software\\Classes\\WOW6432Node\\CLSID\\{04271989-C4D2-8D26-E559-3BE1753BC3ED}");
+            DeleteKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{04271989-C4D2-8D26-E559-3BE1753BC3ED}");
+            DeleteKey("Software\\Microsoft\\Office\\Teams\\LoggedInOnce");
+            DeleteKey("Software\\Microsoft\\Office\\Teams\\HomeUserUpn");
+            DeleteKey("Software\\Microsoft\\Office\\Teams\\DeadEnd");
+            DeleteKey("Software\\Microsoft\\Office\\Outlook\\Addins\\TeamsAddin.FastConnect");
+
+
+            AddOutlookKeys();
+
+            AddTeamsKeys();
+
+            MoveOneDriveFiles();
+        }
+
+
+        private void MoveOneDriveFiles()
+        {
+
+            string Targettenantname = "Iveco Group";
+
+            string ProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string OneDriveTargetPath = ProfilePath + "\\OneDrive - " + Targettenantname;
+
+            System.IO.Directory.CreateDirectory(OneDriveTargetPath);
+            WriteLog("Created target OneDrive folder", "INFO");
+            var oneDrivesourcePath = Environment.GetEnvironmentVariable("OneDriveCommercial");
+
+
+            if (oneDrivesourcePath != null)
+            {
+                Directory.Move(oneDrivesourcePath, OneDriveTargetPath);
+            }
+
+
+        }
+
+        public string SessionLogPath;
+            
+        
+        private void InitializeLogs()
+        {
+            string folderName = @".\Logs";
+            // If directory does not exist, create it
+            if (!Directory.Exists(folderName))
+            {
+                Directory.CreateDirectory(folderName);
+            }
+
+            SessionLogPath = folderName + "\\ExecutionLog." + DateTime.Now.ToString("yyyyMMddHHmm") + ".log";
+
+            using (StreamWriter sw = File.AppendText(SessionLogPath))
+            {
+                sw.WriteLine("TimeStamp;Severity;Message");
+            }
+
+            WriteLog("Transiction Script started", "INFO");
+        }
+
+
+        private void WriteLog(string message,string severity) 
+        {
+            string path = SessionLogPath;
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyyMMddHHMMss") + ";" + severity +";" + message);
+            }
+        }
+        
+
+
+        private void DeleteKey (string keyf)
+        {
+            try
+            {
+                Registry.CurrentUser.DeleteSubKeyTree(keyf);
+                WriteLog("Deleted key " + keyf, "INFO");
+            } catch (Exception e)
+            {
+                WriteLog("Failed to delete key " + keyf , "WARNING");
+            }
+        }
+
+
+        private void AddOutlookKeys()
+        {
+            try
+            {
+
+                Microsoft.Win32.RegistryKey Teamsmykey;
+                Teamsmykey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Office\\Teams");
+                Teamsmykey.SetValue("SkipUpnPrefill", "1");
+                Teamsmykey.Close();
+
+
+                Microsoft.Win32.RegistryKey OutlookKeys;
+                OutlookKeys = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Office\\16.0\\Outlook\\Profiles\\TSClientProfile");
+                OutlookKeys.Close();
+
+
+                Microsoft.Win32.RegistryKey OutlookKeys2;
+                OutlookKeys2 = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Office\\16.0\\Outlook");
+                OutlookKeys2.SetValue("DefaultProfile", "TSClientProfile");
+                OutlookKeys2.Close();
+
+
+                WriteLog("Create required keys for Outlook", "INFO");
+            }
+            catch (Exception e)
+            {
+                WriteLog("Failed to add Outlook keys", "ERROR");
+            }
+        }
+
+
+
+        private void AddTeamsKeys()
+        {
+            try
+            {
+
+                Microsoft.Win32.RegistryKey Teamsmykey;
+                Teamsmykey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Office\\Teams");
+                Teamsmykey.SetValue("SkipUpnPrefill", "1");
+                Teamsmykey.Close();
+
+                WriteLog("Create required keys for Teams", "INFO");
+            }
+            catch (Exception e)
+            {
+                WriteLog("Failed to add Teams", "ERROR");
+            }
+        }
+
+
+    }
+}
